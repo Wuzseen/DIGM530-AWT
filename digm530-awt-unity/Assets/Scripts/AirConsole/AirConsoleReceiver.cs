567c3c7a43fd34af0c8097affce5ad34ecee663b
Timothy Day <the.tday@gmail.com>
2016-02-25 23:05:11 -0500

    Basic message on start up

--- digm530-awt-unity/Assets/Scripts/AirConsole/AirConsoleReceiver.cs
+++ digm530-awt-unity/Assets/Scripts/AirConsole/AirConsoleReceiver.cs
@@ -4,16 +4,25 @@ using NDream.AirConsole;
 using Newtonsoft.Json.Linq;
 
 public class AirConsoleReceiver : MonoBehaviour {
-
+	private int narratorId;
 	// Use this for initialization
 	void Start () 
 	{
 		AirConsole.instance.onMessage += AirConsole_instance_onMessage;
+		AirConsole.instance.onConnect += AirConsole_instance_onConnect;
 	}
 
 	void OnDestroy () 
 	{
 		AirConsole.instance.onMessage -= AirConsole_instance_onMessage;
+		AirConsole.instance.onConnect -= AirConsole_instance_onConnect;
+	}
+
+	void AirConsole_instance_onConnect (int device_id)
+	{
+		this.narratorId = device_id;
+
+		print(device_id);
 	}
 
 	void AirConsole_instance_onMessage (int from, JToken data)
