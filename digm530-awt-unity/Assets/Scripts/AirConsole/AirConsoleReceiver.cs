c769a799540cb1b925201082cb0e9cbd3b3ea85f
Timothy Day <the.tday@gmail.com>
2016-02-25 22:38:58 -0500

    Air console set up

---
+++ digm530-awt-unity/Assets/Scripts/AirConsole/AirConsoleReceiver.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class AirConsoleReceiver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		AirConsole.instance.onMessage += AirConsole_instance_onMessage;
+	}
+
+	void OnDestroy () 
+	{
+		AirConsole.instance.onMessage -= AirConsole_instance_onMessage;
+	}
+
+	void AirConsole_instance_onMessage (int from, JToken data)
+	{
+		AirConsole.instance.Message(from, "Full of pixels");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
